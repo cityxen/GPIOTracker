54441becb5f86dea7a04521ab4b9380794d85488
sethcoder <defectiveseth@gmail.com>
2020-11-06 08:38:07 -0500

    Got rid of extraneous folder

--- Build.bat
+++ Build.bat
@@ -3,5 +3,5 @@ dasm i2c.s -oi2c.ml
 copy Kickass.cfg E:\pc_tools\KickAssembler\Kickass.cfg
 java -jar E:\pc_tools\KickAssembler\KickAss.jar gpiotracker.asm
 mkdir X:\temp\prg_xfer\GPIOTracker
-xcopy ..\* X:\temp\prg_xfer\GPIOTracker\* /Y /S
-pause
+xcopy * X:\temp\prg_xfer\GPIOTracker\* /Y /S
+
