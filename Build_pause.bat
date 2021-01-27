287a0f6012e6ce0c7fdcbdc618f299dd91b8b273
deadline-cxn <deadline@cityxen.net>
2021-01-27 05:58:01 -0500

    rm Build_pause.bat

--- Build_pause.bat
+++
@@ -1,7 +0,0 @@
-dasm i2c.s -oi2c.ml
-copy Kickass.cfg E:\pc_tools\KickAssembler\Kickass.cfg
-java -jar E:\pc_tools\KickAssembler\KickAss.jar gpiotracker.asm
-@echo off
-rem mkdir X:\temp\prg_xfer\GPIOTracker
-rem xcopy * X:\temp\prg_xfer\GPIOTracker\* /Y /S
-pause
