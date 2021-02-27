2fe749c0b40d0b8bf15cbc82dabc74dc7abfacff
deadline-cxn <deadline@cityxen.net>
2021-02-27 05:46:35 -0500

    Added VICE compatible version

---
+++ Build-vice.bat
@@ -0,0 +1,10 @@
+rem Build Dorktronic i2c.s
+dasm i2c-vice.s -oi2c.ml -f3 -si2c.symbols
+
+rem Build using retro-dev-tools ( see: https://github.com/cityxen/retro-dev-tools )
+start /b genkickass-script.bat -t C64 -o prg_files_vice -m true -s true -l "RETRO_DEV_LIB"
+KickAss.bat gpiotracker.asm
+
+rem Alternatively, use your own build location
+rem java -jar PATH:\to\your\KickAss\KickAss.jar gpiotracker.asm
+@echo off
